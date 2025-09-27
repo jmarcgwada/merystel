de8aa200d395005172f13b6e9d206ae78ac6db68
jean marc sp <jeanmarc.sp@gmail.com>
2025-09-27 11:43:43 +0000

    dans la page Gestion peux-tu isoler le menu de gauche contenant l'ensemb

--- src/app/management/layout.tsx
+++ src/app/management/layout.tsx
@@ -10,9 +10,17 @@ export default function ManagementLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8 grid md:grid-cols-[200px_1fr] gap-8">
-      <ManagementSideNav />
-      <div>{children}</div>
+    <div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
+      <div className="flex flex-col md:flex-row gap-8">
+        <aside className="md:w-52 lg:w-56 shrink-0">
+          <div className="sticky top-24">
+            <ManagementSideNav />
+          </div>
+        </aside>
+        <main className="flex-1 min-w-0">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
