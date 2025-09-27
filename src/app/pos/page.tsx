e661d74e24296ad3336bbc76b7008cf5b75e20ab
jean marc sp <jeanmarc.sp@gmail.com>
2025-09-27 14:18:20 +0000

    par contre tu m'as inserer un ascenseur à droite de la page je veux que

--- src/app/pos/page.tsx
+++ src/app/pos/page.tsx
@@ -78,7 +78,7 @@ export default function PosPage() {
   return (
     <>
       <div className="h-[calc(100vh-4rem)] bg-muted/40 grid grid-cols-1 md:grid-cols-12 overflow-hidden">
-          <div className="md:col-span-3 lg:col-span-2 border-r bg-card h-full flex flex-col overflow-hidden">
+          <div className="md:col-span-3 lg:col-span-2 border-r bg-card flex flex-col overflow-hidden">
             <CategoryList
               selectedCategory={selectedCategory}
               onSelectCategory={handleSelectCategory}
@@ -88,7 +88,7 @@ export default function PosPage() {
           </div>
 
           <div className={cn(
-            "md:col-span-5 lg:col-span-6 flex flex-col h-full transition-opacity overflow-hidden",
+            "md:col-span-5 lg:col-span-6 flex flex-col transition-opacity overflow-hidden",
              isKeypadOpen && 'opacity-50 pointer-events-none'
           )}>
             <div className="p-4 border-b bg-card">
@@ -139,7 +139,7 @@ export default function PosPage() {
             </ScrollArea>
           </div>
 
-          <div className="md:col-span-4 lg:col-span-4 border-l bg-card h-full overflow-hidden">
+          <div className="md:col-span-4 lg:col-span-4 border-l bg-card flex flex-col overflow-hidden">
             <OrderSummary />
           </div>
       </div>
