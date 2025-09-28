6e79d404486e7e76863b7170640f248751e24a48
jean marc sp <jeanmarc.sp@gmail.com>
2025-09-28 02:42:56 +0000

    il ne faut surtout pas oublier que en point de vente le fonctionnement s

--- src/app/pos/components/checkout-modal.tsx
+++ src/app/pos/components/checkout-modal.tsx
@@ -1,4 +1,5 @@
 
+
 'use client';
 
 import React, { useState, useEffect, useMemo, useRef, useCallback } from 'react';
@@ -421,4 +422,3 @@ export function CheckoutModal({ isOpen, onClose, totalAmount }: CheckoutModalPro
   );
 }
 
-    
