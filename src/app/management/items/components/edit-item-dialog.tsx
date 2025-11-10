c7c30a576e90a620774938cc992a1c4d8db9bea4
jean marc sp <jeanmarc.sp@gmail.com>
2025-11-10 11:37:29 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/management/items/components/edit-item-dialog.tsx
+++ src/app/management/items/components/edit-item-dialog.tsx
@@ -15,6 +15,7 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Switch } from '@/components/ui/switch';
 import { Textarea } from '@/components/ui/textarea';
